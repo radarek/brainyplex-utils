c13e7f2515ba7e8d0a893ad4a5c20a230fd77b1d
Radosław Bułat <radek.bulat@gmail.com>
2013-10-20 18:35:19 +0200

    Added simplecov gem to generate coverage reports.
    Usage:
      COVERAGE=1 bundle exec rake test
      COVERAGE=1 ruby -Ispec/ spec/unit/...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,11 @@ $:.unshift File.expand_path('..', __FILE__)
 $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'rubygems'
+if %w(1 true).include?(ENV['COVERAGE'])
+  require 'simplecov'
+  SimpleCov.start
+end
+
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'pry'
